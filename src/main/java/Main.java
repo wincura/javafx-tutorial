cfacd165a0be86434edc6a572015257b62943e0a
wincura <winchocolate1911@gmail.com>
2026-02-15 22:49:51 +0800

    Start GUI development

---
+++ src/main/java/Main.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * A GUI for Duke using FXML.
+ */
+public class Main extends Application {
+
+    private Duke duke = new Duke();
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+
+            stage.setTitle("InvictaBot");
+
+            Image icon = new Image("/images/icon.png");
+            stage.getIcons().add(icon);
+
+
+            stage.setScene(scene);
+            stage.setMinHeight(250);
+            stage.setMinWidth(420);
+            fxmlLoader.<MainWindow>getController().setDuke(duke);  // inject the Duke instance
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
